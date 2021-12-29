d5769e2a431c1d3279f501d628ec20e282705db1
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2021-12-29 17:52:00 +0100

    added first laserwave emacs config in init.el

--- init.el
+++ init.el
@@ -26,7 +26,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (auto-complete use-package))))
+ '(package-selected-packages
+   (quote
+    (all-the-icons-ivy doom-themes doom-modeline helpful counsel ivy-rich which-key rainbow-delimiters auto-complete use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,3 +36,78 @@
  ;; If there is more than one, they won't work right.
  )
 (ac-config-default)
+
+;; add line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; better parentheses
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; describe keys
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.6))
+
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; description for counsel M-x
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; binding counsel M-x so it will be used instead of the normal M-x
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; dont start searches with ^
+
+;; better description
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+
+
+
+;; Themes
+;; TODOO: The icons in the modeline are not being displayed correctly
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package doom-themes
+  :init (load-theme 'doom-laserwave t))

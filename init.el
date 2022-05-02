50638278208557a41036c06da7877b39ce4ea45d
VJMReichenbach <Valentin.Reichenbach@live.com>
2022-05-02 16:36:58 +0200

    removed java support cuz it was buggy

--- init.el
+++ init.el
@@ -135,27 +135,29 @@
 
 
 ;; java support
-(require 'lsp-java)
-(add-hook 'java-mode-hook #'lsp)
+;; not working -- i am too lazy to fix it
 
-(add-hook 'java-mode-hook #'lsp-java-boot-lens-mode)
-
-(use-package projectile)
-(use-package flycheck)
-(use-package yasnippet :config (yas-global-mode))
-(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :config (setq lsp-completion-enable-additional-text-edit nil))
-(use-package hydra)
-(use-package company)
-(use-package lsp-ui)
-(use-package which-key :config (which-key-mode))
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
-(use-package dap-java :ensure nil)
-(use-package helm-lsp)
-(use-package helm
-  :config (helm-mode))
-(use-package lsp-treemacs)
+;; (require 'lsp-java)
+;; (add-hook 'java-mode-hook #'lsp)
+;; 
+;; (add-hook 'java-mode-hook #'lsp-java-boot-lens-mode)
+;; 
+;; (use-package projectile)
+;; (use-package flycheck)
+;; (use-package yasnippet :config (yas-global-mode))
+;; (use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
+;;   :config (setq lsp-completion-enable-additional-text-edit nil))
+;; (use-package hydra)
+;; (use-package company)
+;; (use-package lsp-ui)
+;; (use-package which-key :config (which-key-mode))
+;; (use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+;; (use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
+;; (use-package dap-java :ensure nil)
+;; (use-package helm-lsp)
+;; (use-package helm
+;;   :config (helm-mode))
+;; (use-package lsp-treemacs)
 
 ;;----------------------------------------------------------Themes------------------------------------------------------------
 

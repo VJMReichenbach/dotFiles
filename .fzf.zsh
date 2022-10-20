1b0283563dc2ee01a13ba717051818720bd06a5a
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2022-10-20 16:45:11 +0200

    fixed the type of .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,1 +1,13 @@
-/home/valentin/.fzf.zsh
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/valentin/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/valentin/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/valentin/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/valentin/.fzf/shell/key-bindings.zsh"

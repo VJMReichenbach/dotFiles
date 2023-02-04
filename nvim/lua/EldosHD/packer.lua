ef8482316aa53efc5fbc2485414d8422afde455f
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2023-02-04 18:56:12 +0100

    first version of nvim config

--- nvim/lua/EldosHD/packer.lua
+++ nvim/lua/EldosHD/packer.lua
@@ -4,14 +4,53 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  } 
+    -- fuzzy finder
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    } 
 
-  use { "ellisonleao/gruvbox.nvim" }
+    -- colorscheme
+    use { "ellisonleao/gruvbox.nvim" } 
+
+    -- treesitter (for syntax highlighting)
+    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+    -- harpoon (for file navigation)
+    use('theprimeagen/harpoon')
+
+    --undotree for undo (duh)
+    use('mbbill/undotree')
+
+    -- git commands
+    use('tpope/vim-fugitive')
+
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+
 end)

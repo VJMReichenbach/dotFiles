799455c14e2fd92103dabec2f473b3054e1b81ef
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2023-02-05 02:05:37 +0100

    added possible options to install.py

--- scripts/install/install.py
+++ scripts/install/install.py
@@ -14,6 +14,12 @@ def installShell():
 if(__name__ == "__main__"):
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=description, epilog=epilog)
 
-
-
+    parser.add_argument("-v", "--version", help="Print version and exit", action="store_true")
+    parser.add_argument("-l", "--list", help="List packages", action="store_true")
+    parser.add_argument("-L", "--link", help="Link dotfiles", action="store_true")
+    parser.add_argument("-a", "--all", help="Install all packages", action="store_true")
+    parser.add_argument("-s", "--shell", help="Install shell related packages", action="store_true")
+    parser.add_argument("-t", "--tools", help="Install general tools", action="store_true")
+    parser.add_argument("-e", "--editors", help="Install all editors that I currently use", action="store_true")
+    parser.add_argument("-m", "--misc", help="Install misc packages", action="store_true")
     args = parser.parse_args()

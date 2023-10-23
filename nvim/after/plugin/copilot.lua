81f27dec982d099d5d820a1d50c87f50bc8be98d
VJMReichenbach <Valentin.Reichenbach@live.com>
2023-10-23 16:24:07 +0200

    removed nvim config

--- nvim/after/plugin/copilot.lua
+++
@@ -1,5 +0,0 @@
-vim.g.copilot_no_tab_map = true
--- vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-vim.keymap.set("i", "<M-i>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-vim.g.copilot_assume_mapped = true
-
--- nvim/lua/EldosHD/init.lua
+++
@@ -1,3 +0,0 @@
-require("EldosHD.remap")
-require("EldosHD.color")
-require("EldosHD.set")

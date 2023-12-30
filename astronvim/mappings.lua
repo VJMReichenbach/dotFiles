65283fe850ac81bcf9bccc24bbbcb7a91169d4c9
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2023-12-30 14:45:30 +0100

    added more mappings for deleting stuff

--- astronvim/mappings.lua
+++ astronvim/mappings.lua
@@ -2,5 +2,9 @@ return {
   n = {
     ["<leader>k"] = { "di{", desc = "delete inner {}" },
     ["<leader>j"] = { "di(", desc = "delete inner ()" },
+    ["<leader>l"] = { "di[", desc = "delete inner []" },
+    ["<leader>h"] = { "di<", desc = "delete inner <>" },
+    ["<leader>\""] = { "di\"", desc = "delete inner double quotes" },
+    ["<leader>'"] = { "di'", desc = "delete inner single quotes" },
   }
 }

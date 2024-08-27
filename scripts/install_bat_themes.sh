55c235a5cb837458898b9566cec71308f123aeee
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2024-08-27 15:41:44 +0200

    fixed bat theme

--- scripts/install_bat_themes.sh
+++ scripts/install_bat_themes.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git clone https://github.com/catppuccin/bat.git
 cd bat
+cd themes
 mkdir -p "$(bat --config-dir)/themes"
 cp *.tmTheme "$(bat --config-dir)/themes"
 bat cache --build
+cd ..
 cd ..
 rm -rf bat

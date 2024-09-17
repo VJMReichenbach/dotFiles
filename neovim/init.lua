a5b68c9a74bab999496e85d8be1e1a92344febb8
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2024-09-17 13:00:21 +0200

    added keybind for opening explorer

--- neovim/init.lua
+++ neovim/init.lua
@@ -80,6 +80,9 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Open explorer
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Open [E]xplorer' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 

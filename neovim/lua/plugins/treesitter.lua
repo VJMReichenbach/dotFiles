88cc0a975ca1cf86dbebbd494731901a80d7194d
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2024-09-23 18:10:51 +0200

    ignored latex in treesitter, using vimtex instead

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust', 'python', 'nu' },
+    ignore_install = { 'latex' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {

1db6618bfd908fce4832f979be244015814eaca9
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2024-11-18 11:07:54 +0100

    added leader+w as write shortcut

--- neovim/lua/plugins/keymaps.lua
+++ neovim/lua/plugins/keymaps.lua
@@ -8,4 +8,15 @@ return {
   set('n', '<leader>h', 'di<', { desc = 'Delete inside <' }),
   set('n', '<leader>"', 'di"', { desc = 'Delete inside "' }),
   set('n', "<leader>'", "di'", { desc = "Delete inside '" }),
+
+  -- which-key show keymaps
+  set(
+    'n',
+    '<leader>?',
+    '<cmd>WhichKey<CR>',
+    { desc = 'Show which-key keymaps' }
+  ),
+
+  -- Save on <leader>w
+  set('n', '<leader>w', '<cmd>w<CR>', { desc = '[W]rite buffer' }),
 }

78941d67e22c11331cb966dffe18a4f1eb67cbcc
VJMReichenbach <Valentin.Reichenbach@live.com>
2024-12-04 13:39:23 +0100

    fmt

--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -32,6 +32,7 @@ return {
   set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' }),
   set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' }),
   set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' }),
+
   -- Delete inside (){}[]<>"'
   set('n', '<leader>j', 'di(', { desc = 'Delete inside (' }),
   set('n', '<leader>k', 'di{', { desc = 'Delete inside {' }),

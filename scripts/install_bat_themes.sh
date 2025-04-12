c9b7f71f551ce134548ebeb9c50d50c413a6f2cf
VJMReichenbach <Valentin.Reichenbach@live.com>
2025-04-12 10:51:43 +0200

    Scripts: added cache build command to bat theme install script

--- scripts/install_bat_themes.sh
+++ scripts/install_bat_themes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p "$(bat --config-dir)/themes"
 wget https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Mocha.tmTheme -O ~/.config/bat/themes/Catppuccin\ Mocha.tmTheme
+bat cache --build

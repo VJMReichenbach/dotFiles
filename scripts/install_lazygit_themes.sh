f578c1eb3fd4e86b112cdab5bf63cc04cdd908d8
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2025-04-12 10:51:19 +0200

    Scripts: added mkdir instruction to lazygit theme install script

--- scripts/install_lazygit_themes.sh
+++ scripts/install_lazygit_themes.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mkdir -p ~/.config/lazygit
 wget https://raw.githubusercontent.com/catppuccin/lazygit/refs/heads/main/themes-mergable/mocha/pink.yml -O ~/.config/lazygit/catppuccin-mocha-pink.yml

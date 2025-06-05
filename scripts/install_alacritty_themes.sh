64aec8fa038defe107ae12cedfa6e19d363bd1ee
VJMReichenbach <Valentin.Reichenbach@live.com>
2025-06-05 17:11:35 +0200

    Scripts: added script for installing alacritty themes

---
+++ scripts/install_alacritty_themes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "###########################"
+echo "Installing Alacritty themes"
+echo "###########################"
+mkdir -p "~/.config/alacritty"
+curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-mocha.toml
+

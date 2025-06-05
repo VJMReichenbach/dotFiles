6f2e8167afc33ad4e7812962ea1dd01f3fc37adf
VJMReichenbach <Valentin.Reichenbach@live.com>
2025-06-05 17:14:59 +0200

    Scripts: added script echo commands

--- scripts/install_btop_theme.sh
+++ scripts/install_btop_theme.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+echo "###########################"
+echo "Installing Btop themes"
+echo "###########################"
 mkdir -p ~/.config/btop/themes
 wget https://github.com/catppuccin/btop/releases/download/1.0.0/themes.tar.gz
 tar -xvf themes.tar.gz

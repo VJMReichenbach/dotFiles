96177c6e8fd983c49794133e977de6e88242b17c
VJMReichenbach <Valentin.Reichenbach@live.com>
2025-06-05 17:14:59 +0200

    Scripts: added script echo commands

--- scripts/install_lazygit_themes.sh
+++ scripts/install_lazygit_themes.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "###########################"
+echo "Installing Lazygit themes"
+echo "###########################"
 mkdir -p ~/.config/lazygit
 wget https://raw.githubusercontent.com/catppuccin/lazygit/refs/heads/main/themes-mergable/mocha/pink.yml -O ~/.config/lazygit/catppuccin-mocha-pink.yml

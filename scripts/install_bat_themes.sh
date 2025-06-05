99c387637803585dee18a261614271d366fba9ba
VJMReichenbach <Valentin.Reichenbach@live.com>
2025-06-05 17:14:59 +0200

    Scripts: added script echo commands

--- scripts/install_bat_themes.sh
+++ scripts/install_bat_themes.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "###########################"
+echo "Installing Bat themes"
+echo "###########################"
 mkdir -p "$(bat --config-dir)/themes"
 wget https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Mocha.tmTheme -O ~/.config/bat/themes/Catppuccin\ Mocha.tmTheme
 bat cache --build

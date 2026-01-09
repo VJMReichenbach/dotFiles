2a1873c4ce76eac417e85e5ddbf7e525e67ed5da
Valentin Reichenbach <Valentin.Reichenbach@live.com>
2026-01-09 11:26:11 +0100

    Neovim: sorted options alphabetically

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,27 +1,28 @@
+vim.g.have_nerd_font = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.opt.termguicolors = true
-vim.opt.clipboard = 'unnamedplus'
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.showmode = false
+
 vim.opt.breakindent = true
-vim.opt.undofile = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
 vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.inccommand = 'split'
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.updatetime = 250
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {

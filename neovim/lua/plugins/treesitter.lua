3318806a32ae15ba452d223d272457777d1dc9a0
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-01-24 12:10:07 +0100

    Neovim: fixed treesitter config name change

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  main = 'nvim-treesitter.config', -- Sets main module to use for opts
   dependencies = {},
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {

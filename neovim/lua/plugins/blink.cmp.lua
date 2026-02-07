6d62feb9f7c48549413ecf9b3645862a08712718
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-02-07 15:34:53 +0100

    Neovim: removed unnecessary comments

--- neovim/lua/plugins/blink.cmp.lua
+++ neovim/lua/plugins/blink.cmp.lua
@@ -2,8 +2,6 @@ return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
-
-  -- use a release tag to download pre-built binaries
   version = '1.*',
 
   ---@module 'blink.cmp'
@@ -15,14 +13,8 @@ return {
     -- C-e: Hide menu
     -- C-k: Toggle signature help (if signature.enabled = true)
     keymap = { preset = 'default' },
-
-    -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = true } },
-
     signature = { enabled = true },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },

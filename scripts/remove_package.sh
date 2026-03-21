79cd54516d3ca0fde8cf6ebbaa18134be094c914
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-03-22 00:10:00 +0100

    Scripts: added script for removing packages

---
+++ scripts/remove_package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Stolen from omarchy: https://github.com/basecamp/omarchy/blob/dev/bin/omarchy-pkg-remove
+
+# Show a fuzzy-finder TUI for picking packages installed on the system to be removed.
+
+fzf_args=(
+  --multi
+  --preview 'yay -Qi {1}'
+  --preview-label='M-p: toggle description, C-j/k: scroll, tab: multi-select'
+  --preview-label-pos='bottom'
+  --preview-window 'down:65%:wrap'
+  --bind 'alt-p:toggle-preview'
+  --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
+  --color 'pointer:red,marker:red'
+)
+
+pkg_names=$(yay -Qqe | fzf "${fzf_args[@]}")
+
+if [[ -n $pkg_names ]]; then
+  # Convert newline-separated selections to space-separated for yay
+  echo "$pkg_names" | tr '\n' ' ' | xargs sudo pacman -Rns --noconfirm
+fi
